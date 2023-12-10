76fd1515e3144d419fa748a7c72ee285a296a9ec
ajiea.chou <1041874421@qq.com>
2023-12-10 11:25:32 +0800

    feat: 添加了divider 方法,学习error 测试内容

--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -2,12 +2,17 @@ package calculator_test
 
 import (
 	"calculator"
+	"math"
 	"testing"
 )
 
 type testCase struct {
 	a, b float64
 	want float64
+}
+
+func closeEnough(a, b float64, tolerance float64) bool {
+	return math.Abs(a-b) <= tolerance
 }
 
 func TestAdd(t *testing.T) {
@@ -52,10 +57,39 @@ func TestMultiply(t *testing.T) {
 	}
 
 	for _, tc := range testCases {
-		got  := calculator.Multiply(tc.a, tc.b)
+		got := calculator.Multiply(tc.a, tc.b)
 
 		if got != tc.want {
 			t.Errorf("Multiply(%v, %v) = %v, want %v", tc.a, tc.b, got, tc.want)
 		}
+	}
+}
+
+func TestDivide(t *testing.T) {
+	t.Parallel()
+	testCases := []testCase{
+		{1, 1, 1},
+		{-1, -1, 1},
+		{10, 2, 5},
+		{1, 3, 0.33333333333333},
+	}
+	for _, tc := range testCases {
+		got, err := calculator.Divide(tc.a, tc.b)
+		if err != nil {
+			t.Fatalf("want no error for the valid input %v", err)
+		}
+		if !closeEnough(got, tc.want, 1e-9) {
+			t.Errorf("Divide(%v, %v) = %v, want %v", tc.a, tc.b, got, tc.want)
+		}
+	}
+
+}
+
+func TestDivideInvalid(t *testing.T) {
+	t.Parallel()
+	_, err := calculator.Divide(1, 0)
+
+	if err == nil {
+		t.Fatalf("want error for the invalid input")
 	}
 }
